4e4e4bb97eb8a19175594b55c11fc7be49ba1d7d
GANTA <GANTA@DESKTOP-7U9EE4O>
2021-07-21 23:41:42 +0530

    First Commit

---
+++ HelloW/src/test/java/com/miraclesoft/HelloW/HelloWApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miraclesoft.HelloW;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
